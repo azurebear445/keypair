3b3e162a24181ad4c3901f2f7518f46b40d7db8f
azurebear445 <awsatlnta@gmail.com>
2026-02-18 15:25:26 -0500

    Add files via upload

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,10 +1,26 @@
+module "resource_group" {
+  source = "../../../."
+
+  environment = "box"
+  location    = "eastus"
+  namespace   = "test-ssh-key"
+
+  tags = {
+    appid              = "app-6138"
+    architecture       = "native"
+    owner              = "cloudoperations@websterbank.com"
+    purpose            = "Used to test the Terraform Resource Group Module."
+    terraform_resource = "true"
+  }
+}
+
 module "ssh_public_key" {
   source = "../../."
 
   environment         = "box"
   location            = "eastus"
   namespace           = "test-ssh-key"
-  resource_group_name = "rg-test-ssh-key-box-eastus"
+  resource_group_name = module.resource_group.name
 
   tags = {
     appid              = "app-6138"
--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -3,11 +3,6 @@ output "ssh_public_key_id" {
   description = "(string) - The ID of the SSH Public Key."
 }
 
-output "ssh_public_key_openssh" {
-  value       = module.ssh_public_key.public_key_openssh
-  description = "(string) - The public key in OpenSSH format."
-}
-
 output "ssh_private_key_pem" {
   value       = module.ssh_public_key.private_key_pem
   sensitive   = true
