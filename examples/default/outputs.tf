a1646fa8c54da66f2b34898d05e44f352472d142
azurebear445 <awsatlnta@gmail.com>
2026-03-02 10:44:49 -0500

    Update outputs.tf

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -1,10 +1,15 @@
-output "ssh_private_key_pem" {
+output "id" {
+  description = "(string) - The ID of the SSH Public Key."
+  value       = module.ssh_public_key.id
+}
+
+output "private_key_pem" {
   description = "(string) - The private key in PEM format."
   sensitive   = true
   value       = module.ssh_public_key.private_key_pem
 }
 
-output "ssh_public_key_id" {
-  description = "(string) - The ID of the SSH Public Key."
-  value       = module.ssh_public_key.id
+output "public_key_openssh" {
+  description = "(string) - The public key in OpenSSH authorized_keys format."
+  value       = module.ssh_public_key.public_key_openssh
 }
