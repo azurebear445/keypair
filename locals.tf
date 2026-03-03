e6b75a52dfa49472d21e3fe278384b7cf3046e18
azurebear445 <awsatlnta@gmail.com>
2026-03-03 15:16:17 -0500

    Add files via upload

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
-  module_name              = "terraform-azurerm-ssh_public_key"
-  module_version           = "0.0.1"
-  location                 = var.location == "eastus" ? var.location : "ncus"
   is_region_eastus         = var.location == "eastus"
   is_region_northcentralus = var.location == "northcentralus"
+  location                 = var.location == "eastus" ? var.location : "ncus"
+  module_name              = "terraform-azurerm-ssh_public_key"
+  module_version           = "0.0.1"
 }
