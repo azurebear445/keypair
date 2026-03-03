f2161857dc3a3394ded51254a741b44fb251d2b3
azurebear445 <awsatlnta@gmail.com>
2026-03-03 09:50:05 -0500

    Add files via upload

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -1,15 +1,15 @@
 output "id" {
-  description = "(string) - The ID of the SSH Public Key."
   value       = module.ssh_public_key.id
+  description = "(string) - The ID of the SSH Public Key."
 }
 
 output "private_key_pem" {
+  value       = module.ssh_public_key.private_key_pem
   description = "(string) - The private key in PEM format."
   sensitive   = true
-  value       = module.ssh_public_key.private_key_pem
 }
 
 output "public_key_openssh" {
-  description = "(string) - The public key in OpenSSH authorized_keys format."
   value       = module.ssh_public_key.public_key_openssh
+  description = "(string) - The public key in OpenSSH authorized_keys format."
 }
